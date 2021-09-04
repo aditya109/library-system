599aaa042842384e670281142cda4f85041555bb
Aditya Kumar <adikid1996@gmail.com>
2021-09-05 01:35:03 +0530

    improving BCH score: short units of code for runSideCar()
    Signed-off-by: Aditya Kumar <adikid1996@gmail.com>

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -2,8 +2,8 @@ package server
 
 import (
 	"fmt"
-	"library-server/internal/app/server/router"
 	"library-server/configs"
+	"library-server/internal/app/server/router"
 	"library-server/pkg/logger"
 	"log"
 	"net/http"
@@ -25,29 +25,34 @@ var errorMessage string
 var err error
 
 func runSideCar() (string, error) {
-	// initializing standard logger
-	standardLogger = logger.NewLogger()
-	// loading configuration
-	filePath, err := filepath.Abs("./configs/config.json")
-
-	if err != nil {
-		return "error while getting config file path", err
-	}
-	filePath = strings.Split(filePath, " <nil>")[0]
-	config, errorMessage, err = configs.LoadConfiguration(filePath, standardLogger)
-	if err != nil {
+	initializeLogger() // initializing standard logger
+	if err = getConfig(); err != nil { 	// getting configs
 		return errorMessage, err
 	}
-
 	serverEnv = new(ServerEnv)
 	if strings.ToUpper(config.EnvironmentType) == "PROD" {
-
 	} else if strings.ToUpper(config.EnvironmentType) == "DEV" {
 		serverEnv.Address = fmt.Sprintf("%s:%s", config.DevEnvs.ServerUri, config.DevEnvs.ServerPort)
 		serverEnv.WriteTimeout = config.DevEnvs.WriteTimeout
 		serverEnv.ReadTimeout = config.DevEnvs.ReadTimeout
 	}
 	return "", nil
+}
+
+func initializeLogger() {
+	standardLogger = logger.NewLogger()
+}
+
+func getConfig() (error) {
+	filePath, err := filepath.Abs("./configs/config.json") // loading configuration
+	if err != nil {
+		return err
+	}
+	config, errorMessage, err = configs.LoadConfiguration(strings.Split(filePath, " <nil>")[0], standardLogger)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func StartServer() {
